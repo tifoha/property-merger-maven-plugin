da2c8124e2d8716513fc68c16e063dc86eb96d6c
tifoha <tifoha@gmail.com>
2016-12-25 21:29:55 +0200

    init

---
+++ src/test/java/ua/tifoha/maven/MergerMojoTest.java
@@ -0,0 +1,16 @@
+package ua.tifoha.maven;
+
+
+//import org.apache.maven.it.Verifier;
+import org.junit.Test;
+
+/**
+ * Created by Vitaliy Sereda on 25.12.16.
+ */
+public class MergerMojoTest {
+//	@Test
+//	public void shouldFillUpParams() throws Exception {
+//		Veri
+//
+//	}
+}
